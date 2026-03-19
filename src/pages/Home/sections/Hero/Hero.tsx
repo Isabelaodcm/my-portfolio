429d06707a8647427010b33d3af5635b2beec3d4
Isabela <isabela.moreira8721@gmail.com>
2026-03-19 11:24:08 -0300

    feat: create Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -4,16 +4,21 @@ import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
-  const StyledHero = styled("div")(() => ({
-    backgroundColor: "black",
+  const StyledHero = styled("div")(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
     height: "100vh", //para q a section ocupe a tela toda
+    display: "flex",
+    alignItems: "center"
   }));
 
-  const StyledImg = styled("img")(() => ({
-    width: "100%",
+  const StyledImg = styled("img")(({theme}) => ({
+    width: "80%",
     borderRadius: "50%", //deixar a imagem redonda
+    border: `1px solid ${theme.palette.primary.contrastText}`
   }));
 
   return (
@@ -22,29 +27,41 @@ const Hero = () => {
         <Container maxWidth="lg">
           {/* <Box sx={{ flexGrow: 1 }}> */}
           <Grid container spacing={2}>
-            <Grid size={{ xs: 12, md: 4 }}>
-              <StyledImg src={Avatar}></StyledImg>
+            <Grid size={{ xs: 12, md: 5 }} >
+              <Box position="relative">
+                <Box position="absolute" width={"150%"} top={-100} right={0} zIndex={1}>
+                  <AnimatedBackground/>
+                </Box>
+                <Box textAlign="center" position="relative" zIndex={2}>
+                  <StyledImg src={Avatar}/>
+                </Box>
+              </Box>
             </Grid>
-            <Grid size={{ xs: 12, md: 8 }}>
-              <Typography color="primary" variant="h1" textAlign="center">
+            <Grid size={{ xs: 12, md: 7 }}>
+              <Typography color="primary.contrastText" variant="h1" textAlign="center">
                 Isabela Oliveira
               </Typography>
-              <Typography color="primary" variant="h2" textAlign="center">
+              <Typography color="primary.contrastText" variant="h2" textAlign="center" pb={2}>
                 I'm a Software Engineer
               </Typography>
-              <Grid container display="flex" justifyContent="center">
+              <Grid container display="flex" justifyContent="center" spacing={3} pb={3}>
                 <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-                  <Button>
-                    <DownloadIcon />
-                    Download CV
-                  </Button>
+
+              <StyledButton> 
+                <DownloadIcon /> 
+                <Typography>
+                Download CV
+                </Typography>
+                </StyledButton>
                 </Grid>
                 <Grid size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-              <Button>
+              <StyledButton>
                 <EmailIcon/>
+                <Typography>
                 Contact me
-              </Button>
 
+                </Typography>
+              </StyledButton>
                 </Grid>
               </Grid>
             </Grid>
